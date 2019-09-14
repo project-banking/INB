2c4be4e4836128a3ae7fea44738fd2ca5eab6e51
EshaAgrawal <52321066+EshaAgrawal@users.noreply.github.com>
2019-09-14 18:52:05 +0530

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,15 @@
+package com.diaspark.INB.service;
+
+import com.diaspark.INB.DTO.LoginUserDTO;
+import com.diaspark.INB.DTO.RegisterUserDTO;
+import com.diaspark.INB.DTO.UserAccountDto;
+import com.diaspark.INB.entity.UserAccount;
+
+public interface UserService {
+    void registerUser(RegisterUserDTO registerUserDTO);
+    String authenticateUser(LoginUserDTO loginUserDTO);
+    UserAccount createAccount(UserAccountDto userAccountDto);
+	public void saveAccount(UserAccountDto userAccountDto);
+
+    
+}
