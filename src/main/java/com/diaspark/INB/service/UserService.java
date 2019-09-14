9eeb1641ceda5aa7c508c2053e9af2828d45e0ee
EshaAgrawal <52321066+EshaAgrawal@users.noreply.github.com>
2019-09-14 18:59:39 +0530

    Delete UserService.java

--- src/main/java/com/diaspark/INB/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.diaspark.INB.service;
-
-import com.diaspark.INB.DTO.LoginUserDTO;
-import com.diaspark.INB.DTO.RegisterUserDTO;
-
-public interface UserService {
-    void registerUser(RegisterUserDTO registerUserDTO);
-    String authenticateUser(LoginUserDTO loginUserDTO);
-}
