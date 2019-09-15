a7416b6af46b9e026a24c47910a31eeef09b9ee2
EshaAgrawal <eshaagrawal277@gmail.com>
2019-09-15 16:19:37 +0530

    Revert "Delete UserController.java"
    This reverts commit 5ded4db04d47a290232e1c2aba2a0323e2b0fda8.

---
+++ src/main/java/com/diaspark/INB/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.diaspark.INB.controller;
+
+import com.diaspark.INB.DTO.LoginUserDTO;
+import com.diaspark.INB.DTO.RegisterUserDTO;
+import com.diaspark.INB.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/registration")
+    public void registration(@RequestBody RegisterUserDTO registerUserDTO) {
+        userService.registerUser(registerUserDTO);
+    }
+
+    @PostMapping("/login")
+    public String authenticate(@RequestBody LoginUserDTO loginUserDTO) {
+        return userService.authenticateUser(loginUserDTO);
+    }
+}
